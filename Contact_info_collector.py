80f990050747301b01a0f75e86e0477e0c85607d
dzc2vr5dft-commits <dzc2vr5dft@privaterelay.appleid.com>
2026-05-17 14:34:37 +0800

    Update Contact_info_collector.py

--- Contact_info_collector.py
+++ Contact_info_collector.py
@@ -2,30 +2,47 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
 st.header("Contact Info Collector")
-
+contact =[]
 with st.form(key="my_form"):
-    contact =[]
+    
     first_name = st.text_input("First name:").title()
     last_name = st.text_input("Last name:").title()
     no = st.number_input("Favorite number:")
     button = st.form_submit_button("Register")
     if button:
-      try:
-        if first_name.strip() == "" or last_name.strip() == "":
-          raise ValueError
-        no = int(no)
+        try:
+            if first_name.strip() == "" or last_name.strip() == "":
+                raise ValueError
+            no = int(no)
     
-      except ValueError:
-        print("Invalid input.")
-      except Exception as i:
-        print(i)
-      else:
-        contact.append({"First Name": first_name, "Last Name": last_name, "Favorite Number": no})
-        data =pd.DataFrame(contact)
-        st.dataframe(data)
-        st.success("Data added successfully!")
+        except ValueError:
+            print("Invalid input.")
+        except Exception as i:
+            print(i)
+        else:
+            contact.append({"First Name": first_name, "Last Name": last_name, "Favorite Number": no})
+        
+            with open ("contacts.csv", "w") as file:
+                writer = csv.DictWriter(file, fieldnames = ["First Name", "Last Name", "Favorite Number"])
+                writer.writeheader()
+                for i in contact:
+                    writer.writerow(i)
+                data = pd.read_csv("contacts.csv")
+                st.success("Data added successfully!")
+            
+                st.dataframe(data)
+          
+
+
+
+
+
+
+
+
 
 
 
