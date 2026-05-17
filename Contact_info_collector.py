83791715095c3a8b56f0243a86beba2a2c8c218a
dzc2vr5dft-commits <dzc2vr5dft@privaterelay.appleid.com>
2026-05-17 14:25:57 +0800

    Update Contact_info_collector.py

--- Contact_info_collector.py
+++ Contact_info_collector.py
@@ -6,25 +6,26 @@ import matplotlib.pyplot as plt
 st.header("Contact Info Collector")
 contact = []
 with st.form(key="my_form"):
-  try:
+  
     first_name = st.text_input("First name:").title()
     last_name = st.text_input("Last name:").title()
     no = st.number_input("Favorite number:")
     button = st.form_submit_button("Register")
     if button:
-      if first_name.strip() == "" or last_name.strip() == "":
-        raise ValueError
-      no = int(no)
+      try:
+        if first_name.strip() == "" or last_name.strip() == "":
+          raise ValueError
+        no = int(no)
     
-  except ValueError:
-    print("Invalid input.")
-  except Exception as i:
-    print(i)
-  else:
-    contact.append({"First Name": first_name, "Last Name": last_name, "Favorite Number": no})
-    data =pd.DataFrame(contact)
-    st.dataframe(data)
-    st.success("Data addedd successfully!")
+      except ValueError:
+        print("Invalid input.")
+      except Exception as i:
+        print(i)
+      else:
+        contact.append({"First Name": first_name, "Last Name": last_name, "Favorite Number": no})
+        data =pd.DataFrame(contact)
+        st.dataframe(data)
+        st.success("Data added successfully!")
 
 
 
