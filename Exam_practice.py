ce20801e845a8991cc112dc4f49d75038e0050a2
dzc2vr5dft-commits <dzc2vr5dft@privaterelay.appleid.com>
2026-05-19 14:47:51 +0800

    Update Exam_practice.py

--- Exam_practice.py
+++ Exam_practice.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import csv
+import time
 
 st.set_page_config("My Streamlit App")
 st.title("Dashboard")
@@ -24,3 +25,44 @@ age = st.number_input("Age", min_value = 1, max_value = 120)
 button =st.button("Submit")
 if button:
   st.success(f"Hello {name}, you are {age} years old.")
+
+col1, col2, col3 = st.columns(3)
+with col1:
+  bt1 = st.button("A")
+
+with col2:
+  bt2 = st.button("B")
+with col3:
+  bt3 = st.button("C")
+
+tab1, tab2 = st.tabs(["Plot", "Data"])
+
+with tab1:
+  st.write("Here goes a chart")
+  st.metric("Sales", f"1000 +5%")
+  placeholder = st.empty()
+  while True:
+    placeholder.write(f"A new number: {np.random.randint(1,11)})
+    time.sleep(2)
+
+with tab2:
+  st.write("Here goes a table.")
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
