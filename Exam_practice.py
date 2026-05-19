7756c199f755dd2407fb0d8b776488c4b45d981b
dzc2vr5dft-commits <dzc2vr5dft@privaterelay.appleid.com>
2026-05-19 14:33:33 +0800

    Create Exam_practice.py

---
+++ Exam_practice.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import csv
+
+st.set_page_config("My Streamlit App")
+st.title("Dashboard")
+st.header("Introduction")
+st.subheader("Key Metrics")
+st.write ("IDK what it does.")
+st.markdown("**Hello** *Bye*")
